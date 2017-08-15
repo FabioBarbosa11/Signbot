0f2bff5acc5814aeff402330913434a06b5abb32
Fábio Barbosa <fabarbosa092@gmail.com>
2017-08-15 23:57:45 +0100

    limpei mais o codigo do test_left_arm_trajetory.cpp. Ainda a fazer testes...

--- signbot_final/signbot_plugins/src/test_left_arm_trajetory.cpp
+++ signbot_final/signbot_plugins/src/test_left_arm_trajetory.cpp
@@ -1,43 +1,14 @@
-/*************************************************
- *  AINDA COM PROBLEMAS!!!!!!!!!!!
- *  O NODO JA PERTENCE AOS GRUPO PUBLISHERS DO TOPICO
- *  MAS AINDA NAO CONSEGUE MANDAR MENSAGEM
- *  MENSAGEM COM ESTRUTRA DIREITA
- * */
 #include "ros/ros.h"
 #include "trajectory_msgs/JointTrajectory.h"
-
-//#include "std_msgs/String.h"
-//using std::string;
 
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "trajectory_test_node");
   ros::NodeHandle nh;
-/*
-  ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
 
-  ros::Rate loop_rate(10);
-  while (ros::ok())
-  {
-    std_msgs::String msg;
-    msg.data = "hello world";
-
-    chatter_pub.publish(msg);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-  }
-  */
-  /*
-  * é so preciso fazer um publish com a mensagem que tera as coordenadas para o /signbot/left_arm/command
-  */
   ros::Rate loop_rate(0.2);
-  //nem criando outro topico isso funciona!!!
   ros::Publisher r_arm_comand_publisher = nh.advertise<trajectory_msgs::JointTrajectory>("/signbot/r_arm_controller/command", 1);
-
   // Create a message to send.
   trajectory_msgs::JointTrajectory msg;
 
@@ -54,14 +25,7 @@ int main(int argc, char **argv)
   // Resize the vector to the same length as the joint names.
   // Values are initialized to 0.
   msg.points[0].positions.resize(msg.joint_names.size(), 1.0);
-  // ********************************** dtestar primeiro na shell e depois por os valores aqui **********************
-  //msg.points[0].positions[0] = 1.0;
-  //msg.points[0].positions[1] = 1.0;
-  //msg.points[0].positions[2] = 1.0;
-  //msg.points[0].positions[3] = 1.0;
-  // How long to take getting to the point (floating point seconds).
   msg.points[0].time_from_start = ros::Duration(1.0);
-
 
   /// Create a JointTrajectory with all positions set to zero, and command the arm.
   while(ros::ok())
